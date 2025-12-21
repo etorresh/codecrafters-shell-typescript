058f45fa7bac3e2454edbaf7b303be6bb675a94e
etorresh <me@etorresh.com>
2025-12-21 08:43:44 -0700

    add echo command

--- app/main.ts
+++ app/main.ts
@@ -6,10 +6,17 @@ const rl = createInterface({
 });
 
 while (true) {
-  const command = await rl.question("$ ");
-  if (command == "exit") {
+  const input = await rl.question("$ ");
+  const args = input.split(" ");
+  const command = args.splice(0, 1)[0];
+  if (command === "exit") {
     break;
   }
-  console.log(`${command}: command not found`); // process.stdout.write(command + ": command not found\n");
+  else if (command === "echo") {
+    console.log(args.join(" "));
+  }
+  else {
+    console.log(`${command}: command not found`); // process.stdout.write(command + ": command not found\n");
+  }
 }
 rl.close();
