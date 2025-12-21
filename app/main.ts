c2d0dd5c80e40df256be4b6c09d20cc63d2215f1
etorresh <me@etorresh.com>
2025-12-21 09:01:10 -0700

    add type command

--- app/main.ts
+++ app/main.ts
@@ -5,15 +5,29 @@ const rl = createInterface({
   output: process.stdout,
 });
 
+const Commands = {
+  EXIT: "exit",
+  ECHO: "echo",
+  TYPE: "type",
+} as const;
+const CommandsValues = new Set(Object.values(Commands));
+
 while (true) {
   const input = await rl.question("$ ");
   const args = input.split(" ");
   const command = args.splice(0, 1)[0];
-  if (command === "exit") {
+  if (command === Commands.EXIT) {
     break;
   }
-  else if (command === "echo") {
+  else if (command === Commands.ECHO) {
     console.log(args.join(" "));
+  }
+  else if(command === Commands.TYPE) {
+    if (CommandsValues.has(command)) {
+      console.log(`${args[0]} is a shell builtin`);
+    } else {[
+      console.log(`${args[0]}: command not found`)
+    ]}
   }
   else {
     console.log(`${command}: command not found`); // process.stdout.write(command + ": command not found\n");
