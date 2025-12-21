0bdb341a72c6bcc8854f15ed1b0f921809321c81
etorresh <me@etorresh.com>
2025-12-21 10:35:08 -0700

    add type PATH search

--- app/main.ts
+++ app/main.ts
@@ -19,6 +19,30 @@ function isCommand(value: string): value is Command {
   return CommandsValues.has(value as Command);
 }
 
+async function type(args: string[]): Promise<string> {
+  if (isCommand(args[0])) {
+    return `${args[0]} is a shell builtin`;
+  } 
+  else {
+    const paths = process.env.PATH?.split(delimiter) ?? [];
+    for (const path of paths) {
+      try {
+        const files = await readdir(path);
+        for (const file of files) {
+          if (file === args[0]) {
+            try {
+              const full_path = `${path}${sep}${args[0]}`;
+              await access(full_path, constants.X_OK);
+              return `${args[0]} is ${full_path}`;
+            } catch {}
+          }
+        }
+      } catch {}
+    }
+    return `${args[0]}: not found`;
+  }
+}
+
 while (true) {
   const input = await rl.question("$ ");
   const args = input.split(" ");
@@ -30,36 +54,7 @@ while (true) {
     console.log(args.join(" "));
   }
   else if(command === Commands.TYPE) {
-    if (isCommand(args[0])) {
-      console.log(`${args[0]} is a shell builtin`);
-    } 
-    else {
-      const paths = process.env.PATH?.split(delimiter) ?? [];
-      let foundCommand = false;
-
-      for (const path of paths) {
-        if (foundCommand) {
-          break;
-        }
-        try {
-          const files = await readdir(path);
-          for (const file of files) {
-            if (file === args[0]) {
-              try {
-                const full_path = `${path}${sep}${args[0]}`;
-                await access(full_path, constants.X_OK);
-                console.log(`${args[0]} is ${full_path}`)
-                foundCommand = true;
-                break;
-              } catch {}
-            }
-          }
-        } catch {}
-      }
-      if (!foundCommand) {
-        console.log(`${args[0]}: not found`)
-      }
-    }
+    console.log(await type(args));
   }
   else {
     console.log(`${command}: command not found`); // process.stdout.write(command + ": command not found\n");
