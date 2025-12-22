a8ea3302f7803d71901ae71259b904db465da475
etorresh <me@etorresh.com>
2025-12-22 07:34:26 -0700

    add external program execution

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,10 @@
 import { createInterface } from "node:readline/promises";
 import { access, constants, readdir } from "node:fs/promises";
 import { delimiter, sep } from "node:path";
+import { execFile } from "node:child_process";
+import { promisify } from "node:util";
+
+const execFileAsync = promisify(execFile);
 
 const rl = createInterface({
   input: process.stdin,
@@ -19,28 +23,23 @@ function isCommand(value: string): value is Command {
   return CommandsValues.has(value as Command);
 }
 
-async function type(args: string[]): Promise<string> {
-  if (isCommand(args[0])) {
-    return `${args[0]} is a shell builtin`;
-  } 
-  else {
-    const paths = process.env.PATH?.split(delimiter) ?? [];
-    for (const path of paths) {
-      try {
-        const files = await readdir(path);
-        for (const file of files) {
-          if (file === args[0]) {
-            try {
-              const full_path = `${path}${sep}${args[0]}`;
-              await access(full_path, constants.X_OK);
-              return `${args[0]} is ${full_path}`;
-            } catch {}
-          }
+async function find_path(target: string): Promise<string | null> {
+  const paths = process.env.PATH?.split(delimiter) ?? [];
+  for (const path of paths) {
+    try {
+      const files = await readdir(path);
+      for (const file of files) {
+        if (file === target) {
+          try {
+            const full_path = `${path}${sep}${target}`;
+            await access(full_path, constants.X_OK);
+            return `${full_path}`;
+          } catch {}
         }
-      } catch {}
-    }
-    return `${args[0]}: not found`;
+      }
+    } catch {}
   }
+  return null;
 }
 
 while (true) {
@@ -54,10 +53,25 @@ while (true) {
     console.log(args.join(" "));
   }
   else if(command === Commands.TYPE) {
-    console.log(await type(args));
+    if (isCommand(args[0])) {
+      console.log(`${args[0]} is a shell builtin`);
+    } else {
+      const path = await find_path(args[0]); 
+      if (path) {
+        console.log(`${args[0]} is ${path}`);
+      } else {
+        console.log(`${args[0]}: not found`);
+      }
+    }
   }
   else {
-    console.log(`${command}: command not found`); // process.stdout.write(command + ": command not found\n");
+    const path = await find_path(command); // we're reusing find_path but we should try to run the file directly as this can cause a data race if we assume there are no changes between checking permissions and executing
+    if (path) {
+      const { stdout, stderr} = await execFileAsync(path, args);
+      process.stdout.write(stdout);
+    } else {
+      console.log(`${command}: command not found`); // process.stdout.write(command + ": command not found\n");
+    }
   }
 }
 rl.close();
