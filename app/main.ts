fc3dcc04616be4ae6d991a3c39bd7e52ba4deedf
etorresh <me@etorresh.com>
2026-01-05 13:12:33 -0700

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -3,6 +3,7 @@ import { access, constants, readdir, writeFile, exists, mkdir } from "node:fs/pr
 import { delimiter, sep } from "node:path";
 import { execFile } from "node:child_process";
 import { promisify } from "node:util";
+import { error } from "node:console";
 
 const execFileAsync = promisify(execFile);
 
@@ -42,13 +43,14 @@ async function find_path(target: string): Promise<string | null> {
   return null;
 }
 
-function parse(input: string): [string[], string | null] {
+function parse(input: string): [string[], number, string | null] {
   const parsedInput: string[] = [];
   let currentChunk: string[] = [];
   let specialChar: "'" | '"' | null = null;
   let escape = false
   let redirection: string[] | null = null;
   let writingTo = parsedInput;
+  let outputType = 0;
   for (let ch of input) {
     if (escape) {
       if (specialChar === '"' && !(['"', "\\", "$", "`"].includes(ch))) {
@@ -73,7 +75,11 @@ function parse(input: string): [string[], string | null] {
       }
     }
     else if (ch === ">" && specialChar === null) {
-      if (currentChunk.length > 0 && !(currentChunk.length === 1 && currentChunk[currentChunk.length - 1] === "1")) {
+      if (currentChunk.length === 0) {
+        outputType = 1;
+      } else if (currentChunk.length === 1 && ["1", "2"].includes(currentChunk[0])) {
+        outputType = parseInt(currentChunk[0]); 
+      } else if (currentChunk.length > 0) {
         writingTo.push(currentChunk.join(""));
       }
       currentChunk = [];
@@ -86,42 +92,60 @@ function parse(input: string): [string[], string | null] {
   }
   writingTo.push(currentChunk.join(""));
   const redirectionPath = redirection === null ? null: redirection.join("");
-  return [parsedInput, redirectionPath];
+  return [parsedInput, outputType, redirectionPath];
 }
 
+type OutputConfig = 
+  | { outputType: 0; redirectionPath: null}
+  | { outputType: 1 | 2; redirectionPath: string};
 class OutputManager {
-  redirectionPath: string | null;
-  constructor(redirectionPath: string | null) {
-    this.redirectionPath = redirectionPath;
+  outputConfig: OutputConfig;
+  constructor(outputType: number, redirectionPath: string | null) {
+    if (outputType === 0 && redirectionPath === null) {
+      this.outputConfig = {outputType, redirectionPath};
+    } else if ((outputType === 1 || outputType === 2) && redirectionPath != null) {
+      this.outputConfig = {outputType, redirectionPath};
+    } else {
+      throw new Error("outputType must be 0, 1, or 2. redirectionPath can not be null when outputType === (1 | 2)");
+    }
   }
 
-  async print(output: string) {
-    if (this.redirectionPath === null) {
-      if (!output.endsWith("\n")) {
-        output += "\n";
-      }
-      process.stdout.write(output);
+  async print(stdout: string, stderr: string = "") {
+    if (stdout.length > 0 && !stdout.endsWith("\n")) {
+      stdout += "\n";
+    }
+    if (stderr.length > 0 && !stderr.endsWith("\n")) {
+      stderr += "\n";
+    }
+
+    if (this.outputConfig.outputType === 0) {
+      process.stdout.write(stdout);
     } else {
-      if (!(await exists(this.redirectionPath))) {
-        const path = this.redirectionPath.split(sep);
+      if (!(await exists(this.outputConfig.redirectionPath))) {
+        const path = this.outputConfig.redirectionPath.split(sep);
         const folderPath = path.slice(0, path.length - 1);
         if (folderPath.length > 0) {
           const folderPathString = folderPath.join(sep);
           await mkdir(folderPathString, {recursive: true});
         }
       }
-      await writeFile(this.redirectionPath, output);
+      if (this.outputConfig.outputType === 1) {
+        await writeFile(this.outputConfig.redirectionPath, stdout);
+        process.stdout.write(stderr);
+      } else if (this.outputConfig.outputType === 2) {
+        await writeFile(this.outputConfig.redirectionPath, stderr);
+        process.stdout.write(stdout);
+      }
     }
   }
 }
 
 while (true) {
   const input = await rl.question("$ ");
-  const [parsedInput, redirectionPath] = parse(input);
+  const [parsedInput, outputType, redirectionPath] = parse(input);
   const command = parsedInput[0];
   const args = parsedInput.slice(1, parsedInput.length);
-  const outputManager = new OutputManager(redirectionPath);
-
+  const outputManager = new OutputManager(outputType, redirectionPath);
 
   if (command === Commands.EXIT) {
     break;
@@ -144,15 +168,18 @@ while (true) {
   else {
     const path = await find_path(command); // I'm reusing find_path but should try to run the file directly as this can cause a data race if I assume there are no changes between checking permissions and executing
     if (path) {
+      let stdout;
+      let stderr;
       try {
-        const { stdout, stderr} = await execFileAsync(command, args);
-        await outputManager.print(stdout);
-        process.stdout.write(stderr);
+        const result = await execFileAsync(command, args);
+        stdout = result.stdout;
+        stderr = result.stderr;
       } catch (err) {
         const e = err as {stdout: string, stderr: string};
-        await outputManager.print(e.stdout);
-        process.stdout.write(e.stderr);
+        stdout = e.stdout;
+        stderr = e.stderr;
       }
+      await outputManager.print(stdout, stderr); // to do: create a function to normalize stdout/stderr for line  buffering as if it doesn't have \n then it doesn't print. Interesting article on it https://www.pixelbeat.org/programming/stdio_buffering/
     } else {
       await outputManager.print(`${command}: command not found`);
     }
